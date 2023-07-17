2436e6f3a848085d1b45122db8be1684b07cbd09
Simon Smulders <simon.smulders@tenzinger.com>
2023-07-17 13:22:46 +0200

    master: added mui joy components

--- src/scenes/MainScene/MainScene.jsx
+++ src/scenes/MainScene/MainScene.jsx
@@ -1,22 +1,35 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { useGetPostsQuery } from "../../slices/api/api";
-import Link from '@mui/joy/Link';
+import { Link, Stack, Sheet, Box } from '@mui/joy';
+import { styled } from '@mui/joy/styles';
+
+const Item = styled(Sheet)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? theme.palette.background.level1 : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  borderRadius: 4,
+  color: theme.vars.palette.text.secondary,
+}));
 
 export default function MainScene () {
 
   const { data: posts } = useGetPostsQuery();
 
   return (
-    <ul>
-      {posts?.map(post => <li key={post.id}>
-          <Link
-            component={RouterLink}
-            to={`/posts/${post.id}`}
-          >
-            {post.title}
-          </Link>
-        </li>)}
-    </ul>
+    <Box sx={{ width: '100%' }}>
+      <Stack>
+        {posts?.map(post => <Item key={post.id}>
+            <Link
+              component={RouterLink}
+              to={`/posts/${post.id}`}
+            >
+              {post.title}
+            </Link>
+          </Item>)}
+      </Stack>
+    </Box>
   )
 
 };
--- src/scenes/PostsScene/PostsScene.jsx
+++ src/scenes/PostsScene/PostsScene.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import papa from 'papaparse';
 import { useGetPostByIdQuery } from '../../slices/api/api';
 import { MyPlayer } from '../../utils';
+import { Box, Typography } from '@mui/joy';
 
 export default function PostsScene () {
 
@@ -27,16 +28,27 @@ export default function PostsScene () {
     fetchParseData()
   }, []);
 
-  const videoId = csvData?.find(item => item[0] === postId)[1];
+  const videoId = useMemo(() => {
+
+    if (!csvData) {
+      return;
+    }
+
+    return csvData?.find(item => item[0] === postId)[1];
+  }, [ csvData, postId ]);
 
   return post && (
     <>
-      <div>
-        {post.title}
-      </div>
-      <div>
-        {post.body}
-      </div>
-      <MyPlayer videoId={videoId} />
+      <Box sx={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography level="h2">
+          {post.title}
+        </Typography>
+        <Typography level='h5'>
+          {post.body}
+        </Typography>
+        <Box sx={{ width: '640px' }}>
+          <MyPlayer videoId={videoId} />
+        </Box>
+      </Box>
     </>
 )};
